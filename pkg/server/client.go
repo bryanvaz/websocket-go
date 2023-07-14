a027eaba354f278e42e3a03c5b278c43fdeb269f
Bryan Vaz <bryanvaz@users.noreply.github.com>
2023-07-14 00:27:09 -0400

    server relays message to all connected clients

--- pkg/server/client.go
+++ pkg/server/client.go
@@ -6,30 +6,57 @@ import (
 	"net/http"
 )
 
-var upgrader = websocket.Upgrader{}
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
 
 // Represents the connection of a single client
-type ClientConnection struct{}
+type ClientConnection struct {
+	conn   *websocket.Conn
+	closed bool
+	// from me to network (msg received from this client)
+	inBound <-chan string
+	// from network to me (msg to send to this client)
+	outBound chan<- string
+}
 
 // Upgrades to WS connection for an HTTP request
-func NewClientConnection(w http.ResponseWriter, r *http.Request) (*ClientConnection, error) {
+func NewClientConnection(
+	w http.ResponseWriter,
+	r *http.Request,
+	s *Server,
+) (*ClientConnection, error) {
 	log.Printf("WS UPGRADE REQUEST (%s)\n", r.RemoteAddr)
-	conn, err := upgrader.Upgrade(w, r, nil)
+	c, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Fatalln("WS UPGRADE FAIL ("+r.RemoteAddr+"): ", err)
 		return nil, err
 	}
 	log.Printf("WS UPGRADE SUCCESS (%s)\n", r.RemoteAddr)
 
-	clientConn := ClientConnection{}
+	// from network to me
+	out := make(chan string, 1)
+
+	// from me to network
+	in := make(chan string, 1)
+
+	client := ClientConnection{
+		conn:     c,
+		closed:   false,
+		inBound:  in,
+		outBound: out,
+	}
 
 	go func() {
 		defer func() {
-			conn.Close()
+			c.Close()
+			s.RemoveClient(&client)
+			client.closed = true
 		}()
 
 		for {
-			mt, msg, err := conn.ReadMessage()
+			mt, msg, err := c.ReadMessage()
 			if err != nil {
 				log.Printf("RECV FAILURE (%s): %s\n", r.RemoteAddr, err)
 				break
@@ -41,19 +68,34 @@ func NewClientConnection(w http.ResponseWriter, r *http.Request) (*ClientConnect
 			}
 			log.Printf("RECV (%s): %s\n", r.RemoteAddr, msg)
 
+			in <- string(msg)
+
 			// Echo message back to client as a cat
-			resp := respGenerator((string(msg)))
-			err = conn.WriteMessage(websocket.TextMessage, []byte(resp))
-			if err != nil {
-				log.Printf("SEND FAILURE (%s): %s\n", r.RemoteAddr, err)
-				break
-				// Should probably break because there's something wrong
-				// with the connection
-			} else {
-				log.Printf("SEND (%s): %s\n", r.RemoteAddr, resp)
+			// resp := respGenerator((string(msg)))
+			// err = c.WriteMessage(websocket.TextMessage, []byte(resp))
+			// if err != nil {
+			// 	log.Printf("SEND FAILURE (%s) (msg: %s): %s\n", r.RemoteAddr, resp, err)
+			// 	break
+			// } else {
+			// 	log.Printf("SEND (%s): %s\n", r.RemoteAddr, resp)
+			// }
+		}
+	}()
+
+	go func() {
+		for msg := range out {
+			if !client.closed {
+				outMsg := "NETWORK: " + msg
+				err = c.WriteMessage(websocket.TextMessage, []byte(outMsg))
+				if err != nil {
+					log.Printf("SEND FAILURE (%s) (msg: %s): %s\n", r.RemoteAddr, outMsg, err)
+					break
+				} else {
+					log.Printf("SEND (%s): %s\n", r.RemoteAddr, outMsg)
+				}
 			}
 		}
 	}()
 
-	return &clientConn, nil
+	return &client, nil
 }
--- pkg/server/server.go
+++ pkg/server/server.go
@@ -4,12 +4,18 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 )
 
-type Server struct{}
+type Server struct {
+	clientList map[*ClientConnection]bool
+	mutex      sync.Mutex
+}
 
 func NewServer() (*Server, error) {
-	server := Server{}
+	server := Server{
+		clientList: make(map[*ClientConnection]bool),
+	}
 
 	return &server, nil
 }
@@ -18,11 +24,38 @@ func respGenerator(input string) string {
 	return fmt.Sprintf("*meow* %s", input)
 }
 
+func (s *Server) AddClient(client *ClientConnection) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	s.clientList[client] = true
+}
+
+func (s *Server) RemoveClient(client *ClientConnection) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	delete(s.clientList, client)
+	log.Println("Client disconnected: ", client.conn.RemoteAddr())
+}
+
 func (s *Server) HandleNewWsConn(w http.ResponseWriter, r *http.Request) {
 
-	_, err := NewClientConnection(w, r)
+	client, err := NewClientConnection(w, r, s)
 	if err != nil {
 		log.Println("Error creating new client connection: ", err)
 	}
+
+	s.AddClient(client)
+	log.Println("New client connected: ", r.RemoteAddr)
+	log.Println("Total clients connected: ", len(s.clientList))
+
+	go func() {
+		for {
+			msg := <-client.inBound
+			// client.outBound <- respGenerator(msg)
+			for destClient := range s.clientList {
+				destClient.outBound <- respGenerator(msg)
+			}
+		}
+	}()
 
 }
